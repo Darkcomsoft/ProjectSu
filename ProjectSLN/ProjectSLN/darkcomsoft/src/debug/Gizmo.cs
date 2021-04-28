04f778c082097e14b4f05cee803de44bf239de9f
Estevão S. Vargas <grodolfs@gmail.com>
2021-04-28 17:05:54 -0300

    improve gizmo rectangle 2d draw, and bind buffer

--- ProjectSLN/ProjectSLN/darkcomsoft/src/debug/Gizmo.cs
+++ ProjectSLN/ProjectSLN/darkcomsoft/src/debug/Gizmo.cs
@@ -15,10 +15,8 @@ namespace Projectsln.darkcomsoft.src.debug
 
         private Shader m_gizmoShader;
 
-        private int[] m_rectangleIndices;
         private Vector2[] m_rectangleVertices;
-        private Vector2[] m_rectangleUv;
-        private int IBO, VAO, VBO, UBO;
+        private int VAO, VBO;
 
         private bool m_gizmoDisponivel = false;
 
@@ -38,9 +36,7 @@ namespace Projectsln.darkcomsoft.src.debug
 
             DisposeBuffers();
 
-            m_rectangleIndices = null;
             m_rectangleVertices = null;
-            m_rectangleUv = null;
             m_gizmoShader = null;
 
             m_instance = null;
@@ -49,14 +45,8 @@ namespace Projectsln.darkcomsoft.src.debug
 
         private void SetUpBuffers()
         {
-            VAO = GL.GenVertexArray();//VertexArray Buffer
-            IBO = GL.GenBuffer();//Index Buffer
-
-            VBO = GL.GenBuffer();//Vertex Buffer
-            UBO = GL.GenBuffer();//Uv Buffer
-
-            GL.BindBuffer(BufferTarget.ElementArrayBuffer, IBO);
-            GL.BufferData(BufferTarget.ElementArrayBuffer, m_rectangleIndices.Length * sizeof(int), m_rectangleIndices, BufferUsageHint.StaticDraw);
+            VAO = GL.GenVertexArray();
+            VBO = GL.GenBuffer();
 
             GL.BindVertexArray(VAO);
 
@@ -64,29 +54,15 @@ namespace Projectsln.darkcomsoft.src.debug
             GL.BufferData(BufferTarget.ArrayBuffer, m_rectangleVertices.Length * Vector2.SizeInBytes, m_rectangleVertices, BufferUsageHint.StaticDraw);
             GL.VertexAttribPointer(0, 2, VertexAttribPointerType.Float, false, 0, 0);
             GL.EnableVertexAttribArray(0);
-
-            GL.BindBuffer(BufferTarget.ArrayBuffer, UBO);
-            GL.BufferData(BufferTarget.ArrayBuffer, m_rectangleUv.Length * Vector2.SizeInBytes, m_rectangleUv, BufferUsageHint.StaticDraw);
-            GL.VertexAttribPointer(1, 2, VertexAttribPointerType.Float, false, 0, 0);
-            GL.EnableVertexAttribArray(1);
         }
 
         private void DisposeBuffers()
         {
-            GL.BindBuffer(BufferTarget.ElementArrayBuffer, 0);
-            GL.BindVertexArray(0);
-
             GL.BindVertexArray(0);
             GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
             GL.DisableVertexAttribArray(0);
 
-            GL.BindVertexArray(0);
-            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
-            GL.DisableVertexAttribArray(0);
-
-            GL.DeleteBuffer(IBO);
             GL.DeleteBuffer(VBO);
-            GL.DeleteBuffer(UBO);
             GL.DeleteVertexArray(VAO);
         }
 
@@ -99,20 +75,6 @@ namespace Projectsln.darkcomsoft.src.debug
                 new Vector2(-1f, -1f), // bottom left
                 new Vector2(-1f,  1f) // top left
            };
-
-            m_rectangleIndices = new int[6]
-            {
-                0, 1, 3,   // first triangle
-                1, 2, 3    // second triangle
-            };
-
-            m_rectangleUv = new Vector2[4] {
-                new Vector2(0.0f, 1.0f),
-                new Vector2(1.0f, 1.0f),
-
-                new Vector2( 1.0f, 0.0f),
-                new Vector2(0.0f, 0.0f)
-            };
         }
 
         public static void DrawCube()
@@ -137,16 +99,13 @@ namespace Projectsln.darkcomsoft.src.debug
             if (m_instance == null) { return; }
             if (!m_instance.m_gizmoDisponivel) { return; }
 
-            color.A = 0.5f;
-
             GL.Enable(EnableCap.Blend);
             GL.Disable(EnableCap.DepthTest);
             GL.BlendFunc(BlendingFactor.SrcAlpha, BlendingFactor.OneMinusSrcAlpha);
 
+            //Draw Start
             GL.BindVertexArray(m_instance.VAO);
-            GL.BindBuffer(BufferTarget.ElementArrayBuffer, m_instance.IBO);
-
-            m_instance.m_gizmoShader.Use();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, m_instance.VBO);
 
             m_instance.m_gizmoShader.Use();
 
@@ -154,10 +113,11 @@ namespace Projectsln.darkcomsoft.src.debug
             m_instance.m_gizmoShader.Set("projection", projection);
             m_instance.m_gizmoShader.Set("gizmoColor", color);
 
-            GL.DrawElements(primitiveType, m_instance.m_rectangleIndices.Length, DrawElementsType.UnsignedInt, 0);
+            GL.DrawArrays(primitiveType, 0, m_instance.m_rectangleVertices.Length);
 
-            GL.BindBuffer(BufferTarget.ElementArrayBuffer, 0);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
             GL.BindVertexArray(0);
+            //Draw End
 
             GL.Enable(EnableCap.DepthTest);
             GL.Disable(EnableCap.Blend);
@@ -167,10 +127,5 @@ namespace Projectsln.darkcomsoft.src.debug
         {
             DrawRectangle(Matrix4.CreateScale(w,h,0) * Matrix4.CreateTranslation(x,y,0), projection, color, primitiveType);
         }
-    }
-
-    public enum GizmoDrawMode
-    {
-        Wire, Full
     }
 }
